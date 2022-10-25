5b0612b6da7f518878889f2314ff9cb5304f44ff
201steve <79638133+201steve@users.noreply.github.com>
2022-10-25 20:13:07 +0900

    ADD: 로그인페이지 UI 완성

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,7 +1,34 @@
 import React from "react";
+import "./login.scss";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="container">
+      <main className="loginContainer">
+        <h1 className="title">Westagram</h1>
+        <form className="loginForm">
+          <input
+            className="userId"
+            name="userId"
+            type="text"
+            placeholder="전화번호, 사용자 이름 또는 이메일"
+          />
+          <input
+            className="userPassword"
+            name="userPassword"
+            type="password"
+            placeholder="비밀번호"
+          />
+          <button className="loginButton" type="button" disabled>
+            로그인
+          </button>
+        </form>
+        <a className="findPassword" href="/">
+          비밀번호를 잊으셨나요?
+        </a>
+      </main>
+    </div>
+  );
 };
 
 export default Login;
